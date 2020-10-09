c8e245b9303bc1638eee00d9df17c388c7a84e9c
jelka2008 <jelka2008.rambler.ru>
2020-10-09 17:44:03 +0300

    feth: solved task human-readable-numbers

--- src/index.js
+++ src/index.js
@@ -1,3 +1,67 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    const numStr = number.toString().split("").reverse().join("");
+    const hundredS = (number / 100) >> 0;
+    const tenS = ((number - hundredS * 100) / 10) >> 0;
+    const unitS = number % 10;
+
+    let rezStr = "";
+
+    const zero = "zero";
+    const arrayOneToNineteen = [
+        "",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+    const arrayTwentyToNinety = [
+        "",
+        "",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+    const hundred = "hundred";
+
+    if (number === 0) {
+        return zero;
+    }
+    if (hundredS !== 0 && number < 1000) {
+        rezStr = arrayOneToNineteen[hundredS] + " " + hundred;
+    }
+    if (number % 100 >= 1 && number % 100 <= 9) {
+        rezStr = rezStr + " " + arrayOneToNineteen[unitS];
+    }
+    if (number % 100 >= 10 && number % 100 <= 19) {
+        rezStr = rezStr + " " + arrayOneToNineteen[number % 100];
+    }
+    if (number % 100 > 19) {
+        rezStr =
+            rezStr +
+            " " +
+            arrayTwentyToNinety[tenS] +
+            " " +
+            arrayOneToNineteen[unitS];
+    }
+    return rezStr.trim();
+};
